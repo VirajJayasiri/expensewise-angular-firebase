5eefb788b274052a01b3c5bc44017b6b66b08e1f
Viraj Jayasiri <viraj.jayasiri.work@gmail.com>
2026-07-27 22:16:44 +0530

    Add Firebase configuration and environment settings
    - Created firebase.config.ts to initialize Firebase app, authentication, and Firestore database.
    - Added environment.ts to store Firebase configuration details for development.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,3 +1,5 @@
+import './firebase.config';
+
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: false,
+
+  firebase: {
+    apiKey: 'AIzaSyDLME7Pp8OWtuJlv9dKB7Hd9dubohSqURM',
+    authDomain: 'expensewise-vdj.firebaseapp.com',
+    projectId: 'expensewise-vdj',
+    storageBucket: 'expensewise-vdj.firebasestorage.app',
+    messagingSenderId: '1056549003737',
+    appId: '1:1056549003737:web:cb5b1b42983065249c39c8',
+  },
+};
