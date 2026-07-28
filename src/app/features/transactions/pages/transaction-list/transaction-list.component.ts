f54b0d57f2ec6a88512d258102618c1bbf12a3ec
Viraj Jayasiri <viraj.jayasiri.work@gmail.com>
2026-07-28 16:26:59 +0530

    Add TransactionList component with HTML, SCSS, TypeScript, and unit tests

---
+++ src/app/features/transactions/pages/transaction-list/transaction-list.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-list',
+  imports: [],
+  templateUrl: './transaction-list.component.html',
+  styleUrl: './transaction-list.component.scss',
+})
+export class TransactionList {}
