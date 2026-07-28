bd70e3d13fcc2aa41d06fab80a67e3878fe6f02e
Viraj Jayasiri <viraj.jayasiri.work@gmail.com>
2026-07-28 16:27:24 +0530

    Add TransactionForm component with HTML, SCSS, TypeScript, and unit tests

---
+++ src/app/features/transactions/pages/transaction-form/transaction-form.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-transaction-form',
+  imports: [],
+  templateUrl: './transaction-form.component.html',
+  styleUrl: './transaction-form.component.scss',
+})
+export class TransactionForm {}
